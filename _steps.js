0e9c4295f8e43090b77007771fd7e3ff50ff5f4d
saiful islam <mr.saiful.dev@gmail.com>
2023-12-03 01:42:11 +0600

    first commit

---
+++ _steps.js
@@ -0,0 +1,19 @@
+
+/* 
+1. create a fooder 
+2. open commnad line that foolder
+3. npm init 
+4. npm install express
+5. create index.js file 
+
+for express 
+--------------
+* require expree 
+* create app variable 
+* declare port 
+* set app.get('/)
+* listen to post 
+* node index.js
+* check your browser for this port   
+
+*/
